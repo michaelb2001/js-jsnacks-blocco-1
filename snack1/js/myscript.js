671ccdec3ef476935773400daff8081943d1f00c
michaelb2001 <brunco.michael@gmail.com>
2021-12-16 12:55:37 +0100

    prima versione snack1

---
+++ snack1/js/myscript.js
@@ -0,0 +1,11 @@
+let myArray = [];
+let somma = 0;
+let i = 0;
+do{
+    let num = parseInt(prompt("inserire un numero: "));
+    myArray[i] = num;
+    somma += myArray[i];
+    i++;
+}while(somma < 50);
+
+document.writeln(somma);
